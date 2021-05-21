989395f9cb2e0605e6f2643694c9c00c394973fa
PX Blue Agent <pxblue@eaton.com>
2021-05-21 14:50:43 +0000

    Automated deployment to GitHub Pages: 5dd9acddef948e9c4b13197771b5102a317df1e9

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.3cec62402023019bb701693276b7aca9.js"
+  "precache-manifest.b9d5e632fb6f7ab4606f8703169873fa.js"
 );
 
 self.addEventListener('message', (event) => {
